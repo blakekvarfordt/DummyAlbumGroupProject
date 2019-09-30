4df12c8edbd1f23286a9046824c027ebb7ac8335
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-09-30 10:39:24 -0600

    initial commit / sets up album model

---
+++ DummyAlbum/DummyAlbum/ModelController/AlbumController.swift
@@ -0,0 +1,9 @@
+//
+//  AlbumController.swift
+//  DummyAlbum
+//
+//  Created by Blake kvarfordt on 9/30/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import Foundation
---
+++ DummyAlbum/DummyAlbum/ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DummyAlbum
+//
+//  Created by Blake kvarfordt on 9/30/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
