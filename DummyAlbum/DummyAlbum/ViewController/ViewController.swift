737df83da81c75b6579132a115192e23663919d3
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-09-30 11:39:16 -0600

    sets up views and imputs data

--- DummyAlbum/DummyAlbum/ViewController/ViewController.swift
+++ DummyAlbum/DummyAlbum/ViewController/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
 
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    var posts = PostController.shared.posts
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
         
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? ViewControllerCVCell else { return UICollectionViewCell() }
+        
+        let post = PostController.shared.posts[indexPath.row]
+        
+        cell.imageView.image = post.image
+        cell.postTitleLabel.text = post.title
+        cell.postCaptionLabel.text = post.caption
+        
+        return cell
+    }
 
 }
 
---
+++ DummyAlbum/DummyAlbum/ViewController/ViewControllerCVCell.swift
@@ -0,0 +1,20 @@
+//
+//  ViewControllerCVCell.swift
+//  DummyAlbum
+//
+//  Created by Blake kvarfordt on 9/30/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerCVCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var postTitleLabel: UILabel!
+    @IBOutlet weak var postCaptionLabel: UILabel!
+    @IBOutlet weak var timestampLabel: UILabel!
+    
+    
+}
